3493923b3b21ec3f0a2628850f9f8582db0f9fda
Enrique Ortiz <oice6703@gmail.com>
2019-07-16 21:33:09 -0500

    Finishing first screen

---
+++ lib/screens/todolist.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/utils/dbhelper.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => TodoListState();
+}
+
+class TodoListState extends State {
+  DbHelper helper = DbHelper();
+  List<Todo> todos;
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    if (todos == null) {
+      todos = List<Todo>();
+      getData();
+    }
+    return Scaffold(
+      body: todoListItems(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        tooltip: "Add new todo",
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView todoListItems() {
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: getColor(this.todos[position].priority),
+              child: Text(
+                this.todos[position].title,
+              ),
+            ),
+            title: Text(this.todos[position].priority.toString()),
+            subtitle: Text(this.todos[position].date),
+            onTap: () {
+              debugPrint("Tapped on " + this.todos[position].id.toString());
+            },
+          ),
+        );
+      },
+      itemCount: count,
+    );
+  }
+
+  void getData() {
+    final dbFuture = helper.initializeDb();
+    dbFuture.then((result) {
+      final todosFuture = helper.getTodos();
+      todosFuture.then((result) {
+        List<Todo> todoList = List<Todo>();
+        count = result.length;
+        for (int i = 0; i < count; i++) {
+          todoList.add(Todo.fromObject(result[i]));
+          debugPrint(todoList[i].title);
+        }
+        setState(() {
+          todos = todoList;
+          count = count;
+        });
+        debugPrint("Items " + count.toString());
+      });
+    });
+  }
+  Color getColor(int priority){
+    switch(priority){
+      case 1:
+        return Colors.red;
+        break;
+      case 2:
+        return Colors.orange;
+        break;
+      case 3:
+        return Colors.green;
+        break;
+      default:
+        return Colors.green;
+        break;
+    }
+  }
+}
